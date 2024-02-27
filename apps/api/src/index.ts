0cf033601806c7db42cd3bd88e17d167d9c7cafb
satoshi <sashafem@mail.ru>
2024-02-27 22:55:40 +0300

    api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize())
 
-app.use(routes);
+app.use('/api', routes);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
